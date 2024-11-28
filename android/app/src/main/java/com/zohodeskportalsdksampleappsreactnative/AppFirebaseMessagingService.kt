a481a29f1655a89d5e7327ed98138b2df24d0538
rajesh.jesh <rajesh.jesh@zohocorp.com>
2024-11-28 12:10:22 +0530

    changed the import statement

--- android/app/src/main/java/com/zohodeskportalsdksampleappsreactnative/AppFirebaseMessagingService.kt
+++ android/app/src/main/java/com/zohodeskportalsdksampleappsreactnative/AppFirebaseMessagingService.kt
@@ -2,14 +2,14 @@ package com.zohodeskportalsdksampleappsreactnative
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import com.zoho.desk.asap.RNZohodeskPortalSDK
+import com.zohodeskportalconfiguration.RNZohoDeskPortalConfigurationModule
 
 class AppFirebaseMessagingService: FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         // to handle push notification default by SDK
-        RNZohodeskPortalSDK.handleNotification(getApplicationContext(), message.data, R.mipmap.ic_launcher);
+        RNZohoDeskPortalConfigurationModule.handleNotification(getApplicationContext(), message.data, R.mipmap.ic_launcher);
     }
 
     override fun onNewToken(token: String) {
