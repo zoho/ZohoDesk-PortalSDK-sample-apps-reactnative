a27b52509e6d42248d6b5b7fd79b48cc2200347c
rajesh.jesh <rajesh.jesh@zohocorp.com>
2024-11-28 12:17:11 +0530

    Added navigation stack and configurations screens

--- App.tsx
+++ App.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { StyleSheet, TouchableOpacity, Text, SafeAreaView, useColorScheme, View, FlatList } from 'react-native';
 import ListScreen from './src/components/ListScreen';
-import { ZohoDeskPortalSDK } from 'react-native-zohodesk-portal-sdk';
+import { ZohoDeskPortalSDK } from '@zohocorp/zohodesk-portal-apikit';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import ConfigurationScreen from './src/components/ConfigurationScreen';
+import { ConfigurationProvider } from './src/components/ConfigurationContext';
 
+const Stack = createNativeStackNavigator();
 function App(): React.JSX.Element {
   
   ZohoDeskPortalSDK.initialise("orgId", "appId", "dc"); // Initializes the ASAP SDK
 
   ZohoDeskPortalSDK.enableLogs();  // use this method to enable logs
   return (
-    <ListScreen />
+    <ConfigurationProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name="Home" component={ListScreen}/>
+          <Stack.Screen 
+            name="Configuration" 
+            component={ConfigurationScreen} 
+            options={({route}) => ({title: route.params.screenName + " Configurations"})}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ConfigurationProvider>
   );
 }
 
