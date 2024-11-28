c4c0b726ff13f9f6caeab8c1de7aaee301f915e1
rajesh.jesh <rajesh.jesh@zohocorp.com>
2024-11-28 11:47:47 +0530

    updated and added new styles

--- src/styles/Styles.js
+++ src/styles/Styles.js
@@ -4,7 +4,47 @@ import Colors from '../constants/Colors';
 import { Dimensions } from 'react-native';
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+    },
+    textViewContainer:{
+      flex: 1,
+      justifyContent: 'center',
+      paddingLeft: 20,
+      paddingRight: 20,
+      paddingTop: 20,
+      paddingBottom: 10,
+    },
+    textFieldTitle:{
+      fontSize: 18,
+      fontWeight: '400',
+      paddingBottom: 4,
+      fontFamily: 'Lato-Bold'
+    },
+    textFieldContainer: {
+      backgroundColor: Colors.white,
+      flexDirection: 'row',
+      alignItems: 'center',
+      borderWidth: 1,
+      borderColor: '#bbdef8', // Border color
+      borderRadius: 5,
+      overflow: 'hidden',
+    },
+    textInput: {
+      flex: 1,
+      height: 45,
+      paddingLeft: 10,
+      fontSize: 16,
+    },
+    showButton: {
+      backgroundColor: '#bbdef8', // Button color
+      paddingVertical: 15,
+      paddingHorizontal: 15,
+      justifyContent: 'center',
+    },
+    showButtonText: {
+      color: Colors.black,
+      fontWeight: 'bold',
+      fontSize: 16,
     },
     rowView: {
         flexDirection: 'row',
@@ -45,8 +85,16 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.white,
         margin: 8,
         borderRadius: 10,
+      }, 
+      iconWrapper: {
+        height: 30,
+        width: 30,
+        position: 'absolute', 
+        right: 2,
+        zIndex: 10,
       },
       image: {
+        position: 'relative',
         height: 120,
         margin: 5,
         borderRadius: 10,
@@ -60,12 +108,20 @@ const styles = StyleSheet.create({
         alignItems: 'center',
       },
       centeredText: {       
-        textAlign: 'center',
-        textAlignVertical: 'center',
         fontSize: 18,
         fontWeight: 'bold',
         color: Colors.darkGray,
+      },
+      centeredContainer: {
         flex: 1,
+        textAlign: 'center',
+        justifyContent: 'center', // Vertical centering
+        alignItems: 'center',     // Horizontal centering
+      },
+      icon: {
+        position: 'absolute', // To place it in the corner
+        top: 10, 
+        right: 10, 
       },
       input: {
         height: 40,
@@ -78,6 +134,21 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
         marginVertical: 20,
       },
+      textField: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+      },
+      title:{
+        paddingTop: 30,
+        paddingLeft: 10,
+        fontSize: 23,
+        fontWeight: 'bold'
+      },
+      fieldTitle:{
+        paddingLeft: 10,
+        fontSize: 14,
+      }
 })
 
 export default styles;
