e5545d307c9153dd548b2d09b4354d47125df13f
rajesh.jesh <rajesh.jesh@zohocorp.com>
2024-11-28 11:41:38 +0530

    removed the import statement

--- ios/zohoDeskPortalSDKSampleAppsReactNative/AppDelegate.h
+++ ios/zohoDeskPortalSDKSampleAppsReactNative/AppDelegate.h
@@ -1,6 +1,6 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
-#import <RNZohodeskPortalSdk/RNZohoDeskPortalSDK.h>
+
 #import <UserNotifications/UserNotifications.h> // Import UserNotifications framework
 
 @interface AppDelegate : RCTAppDelegate <UNUserNotificationCenterDelegate> // Conform to UNUserNotificationCenterDelegate
