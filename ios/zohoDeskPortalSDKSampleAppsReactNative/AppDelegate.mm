e07aab14fb76d7bed4e7ddc21e1cf0be5806d766
rajesh.jesh <rajesh.jesh@zohocorp.com>
2024-11-28 11:44:21 +0530

    added model presentation style implementation

--- ios/zohoDeskPortalSDKSampleAppsReactNative/AppDelegate.mm
+++ ios/zohoDeskPortalSDKSampleAppsReactNative/AppDelegate.mm
@@ -1,7 +1,9 @@
 #import "AppDelegate.h"
-
+#import "CustomLightTheme.h"
+#import "CustomDarkTheme.h"
 #import <React/RCTBundleURLProvider.h>
-#import <RNZohodeskPortalSdk/RNZohoDeskPortalSDK.h>
+#import "RNZohoDeskPortalConfiguration.h"
+#import "RNZohoDeskPortalAPIKit.h"
 
 @implementation AppDelegate
 
@@ -17,6 +19,9 @@
   CustomDarkTheme *myCustomDarkTheme = [[CustomDarkTheme alloc] init];
   [RNZohoDeskPortalConfiguration updateLightTheme:myCustomLightTheme];//For light
   [RNZohoDeskPortalConfiguration updateDarkTheme:myCustomDarkTheme];// For Dark
+  
+  ///To use the below  code to set the modal presentation style for SDKs screens
+  [RNZohoDeskPortalConfiguration setModalPresentationStyle:UIModalPresentationFullScreen];
   
   ///To set the Custom font for ASAP SDK to use the below  code
   ///And also verify the font assets are added in the react-native project and linked
@@ -45,13 +50,13 @@
 //    NSString *hexToken = [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x",
 //                          tokenBytes[0], tokenBytes[1], tokenBytes[2], tokenBytes[3],
 //                          tokenBytes[4], tokenBytes[5], tokenBytes[6], tokenBytes[7]];
-//    [RNZohoDeskPortalSDK setDeviceIDForZDPortal:hexToken];
+//  [RNZohoDeskPortalSDK setDeviceIDForNotification:hexToken];
   
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 {
   // use this method to handle push notification
-  [RNZohoDeskPortalSDK processRemoteNotification:userInfo];
+ 
 }
 @end
